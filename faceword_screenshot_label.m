96af2d9cbf38ca1b15f21baabe818ee392f59d45
Haiyang <haiyang.jin@nyu.edu>
2019-12-04 09:11:26 +0400

    add labels for Lateral Occipital Cortex

--- faceword_screenshot_label.m
+++ faceword_screenshot_label.m
@@ -1,3 +1,4 @@
+clear all
 
 boldname = 'self';
 path_output = '~/Desktop/Label_Screenshots';
@@ -7,70 +8,25 @@ subjBoldPath = fullfile(FS.subjects, '..', 'Data_fMRI');
 subjBoldDir = dir(fullfile(subjBoldPath, ['*' boldname]));
 nSubj = numel(subjBoldDir);
 
-
-%% visualize multiple labels at the same time
-fn_multilabels = {'roi.lh.f13.w-vs-o.label', 'roi.lh.f13.f-vs-o.label';
-    'roi.lh.f13.w-vs-o.label', 'roi.lh.f20.f-vs-o.label';
-    'roi.lh.f13.w-vs-o.label', 'roi.lh.f13.f-vs-o.ffa1.label';
-    'roi.lh.f13.w-vs-o.label', 'roi.lh.f13.f-vs-o.ffa2.label'};
-
-nMultilabels = size(fn_multilabels, 1);
-
-f_multi = waitbar(0, 'Generating screenshots for multitple labels...');
-
-overlap = table;
-for iLabel = 1:nMultilabels
-    
-    theseLabel = fn_multilabels(iLabel, :);
-    [hemi, nHemi] = fs_hemi_multi(theseLabel);
-    
-    % move to next loop if the labels are not for the same hemisphere
-    if nHemi ~= 1
-        continue;
-    end
-    
-    for iSubj = 1:nSubj
-        
-        thisSubj = subjBoldDir(iSubj).name;
-        subjCode = fw_subjcode(thisSubj);
-        
-        % Generate the screenshots
-        isok = fs_screenshot_label(subjCode, theseLabel, path_output);
-        
-        if isok
-            % get info about the two labels (their overlaps)
-            overlap_table = fw_labeloverlap(theseLabel, subjCode);
-            overlap = [overlap; overlap_table]; %#ok<AGROW>
-        end
-        
-        % waitbar
-        wait_per = ((iLabel-1) * nSubj + iSubj) / (nMultilabels * nSubj);
-        waitbar(wait_per, f_multi);
-        
-    end
-end
-
-close(f_multi);
-fn_overlap = fullfile(path_output, 'overlap.csv');
-writetable(overlap, fn_overlap);
-
-
 %% visualize every label separately
 labelNames = { % ...
-    'roi.lh.f13.f-vs-o.ffa1.label', ...
-    'roi.lh.f20.f-vs-o.ffa1.label', ...
-    'roi.lh.f13.f-vs-o.ffa2.label',...
-    'roi.lh.f13.w-vs-o.label', ...
-    'roi.lh.f13.f-vs-o.label', ...
-    'roi.lh.f20.f-vs-o.label', ...
-    'roi.lh.f40.f-vs-o.label',...
-    'roi.rh.f13.f-vs-o.ffa1.label', ...
-    'roi.rh.f13.f-vs-o.ffa2.label',...
-    'roi.rh.f20.f-vs-o.ffa2.label',...
-    'roi.rh.f40.f-vs-o.ffa2.label', ...
-    'roi.rh.f13.f-vs-o.label',...
-    'roi.rh.f20.f-vs-o.label', ...
-    'roi.rh.f40.f-vs-o.label'};
+    'roi.lh.f13.o-vs-scr.label'...
+    'roi.rh.f13.o-vs-scr.label'...
+    'roi.lh.f13.f-vs-o.ffa1.label'...
+    'roi.lh.f20.f-vs-o.ffa1.label'...
+    'roi.lh.f13.f-vs-o.ffa2.label'...
+    'roi.lh.f13.w-vs-o.label'...
+    'roi.lh.f13.f-vs-o.label'...
+    'roi.lh.f20.f-vs-o.label'...
+    'roi.lh.f40.f-vs-o.label'...
+    'roi.rh.f13.f-vs-o.ffa1.label' ...
+    'roi.rh.f13.f-vs-o.ffa2.label'...
+    'roi.rh.f20.f-vs-o.ffa2.label'...
+    'roi.rh.f40.f-vs-o.ffa2.label'...
+    'roi.rh.f13.f-vs-o.label'...
+    'roi.rh.f20.f-vs-o.label'...
+    'roi.rh.f40.f-vs-o.label'
+    };
 nLabels = numel(labelNames);
 
 f_single = waitbar(0, 'Generating screenshots for every label...');
@@ -80,8 +36,8 @@ for iLabel = 1:nLabels
     thisLabel = labelNames{iLabel};
     hemi = fs_hemi(thisLabel);
     
-    conStrPosition = strfind(thisLabel, '-') + [-1, 1];
-    theContrast = thisLabel(conStrPosition(1):conStrPosition(2));
+    conStrPosition = strfind(thisLabel, '.');
+    theContrast = thisLabel(conStrPosition(3)+1:conStrPosition(4)-1);
     
     for iSubj = 1:nSubj
         
@@ -107,5 +63,52 @@ end
 
 close(f_single);
 
+
+%% visualize multiple labels at the same time
+% fn_multilabels = {'roi.lh.f13.w-vs-o.label', 'roi.lh.f13.f-vs-o.label';
+%     'roi.lh.f13.w-vs-o.label', 'roi.lh.f20.f-vs-o.label';
+%     'roi.lh.f13.w-vs-o.label', 'roi.lh.f13.f-vs-o.ffa1.label';
+%     'roi.lh.f13.w-vs-o.label', 'roi.lh.f13.f-vs-o.ffa2.label'};
+% 
+% nMultilabels = size(fn_multilabels, 1);
+% 
+% f_multi = waitbar(0, 'Generating screenshots for multitple labels...');
+% 
+% overlap = table;
+% for iLabel = 1:nMultilabels
+%     
+%     theseLabel = fn_multilabels(iLabel, :);
+%     [hemi, nHemi] = fs_hemi_multi(theseLabel);
+%     
+%     % move to next loop if the labels are not for the same hemisphere
+%     if nHemi ~= 1
+%         continue;
+%     end
+%     
+%     for iSubj = 1:nSubj
+%         
+%         thisSubj = subjBoldDir(iSubj).name;
+%         subjCode = fw_subjcode(thisSubj);
+%         
+%         % Generate the screenshots
+%         isok = fs_screenshot_label(subjCode, theseLabel, path_output);
+%         
+%         if isok
+%             % get info about the two labels (their overlaps)
+%             overlap_table = fw_labeloverlap(theseLabel, subjCode);
+%             overlap = [overlap; overlap_table]; %#ok<AGROW>
+%         end
+%         
+%         % waitbar
+%         wait_per = ((iLabel-1) * nSubj + iSubj) / (nMultilabels * nSubj);
+%         waitbar(wait_per, f_multi);
+%         
+%     end
+% end
+% 
+% close(f_multi);
+% fn_overlap = fullfile(path_output, 'overlap.csv');
+% writetable(overlap, fn_overlap);
+% 
 
 
