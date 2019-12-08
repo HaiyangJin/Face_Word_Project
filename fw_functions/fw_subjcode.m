a19ed46aa7b6585c0d8da6ec065807f75c3cf1b3
Haiyang <haiyang.jin@nyu.edu>
2019-12-08 15:31:01 +0400

    rename subjCode_fmri as subjCode

--- fw_functions/fw_subjcode.m
+++ fw_functions/fw_subjcode.m
@@ -1,4 +1,4 @@
-function subjCode_mri = fw_subjcode(subjCode_bold)
+function subjCode = fw_subjcode(subjCode_bold)
 % This function is specific for FaceWord project.
 % This function get the subjCode for $SUBJECTS_DIR in FreeSurfer
 %
@@ -11,6 +11,6 @@ subjCode = subjCode_cell{1};
 fs = fs_setup;
 subjdir = dir(fullfile(fs.subjects, [subjCode '*']));
 
-subjCode_mri = subjdir.name;
+subjCode = subjdir.name;
 
 end
