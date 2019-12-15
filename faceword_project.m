2ecb4624abefcc462dc31d177c015def98e22bb3
Haiyang <haiyang.jin@nyu.edu>
2019-12-15 14:18:56 +0400

    update codes used for the analysis
    add codes for classification
    add codes for searchlight

--- faceword_project.m
+++ faceword_project.m
@@ -61,8 +61,8 @@ fs_fun_screenshot_label(FW, labelList, output_path);
 
 % screenshots of multiple lables
 label_multi = {
-%     'roi.lh.f13.w-vs-o.label', 'roi.lh.f13.f-vs-o.label', 'roi.lh.f20.f-vs-w.label';
-%     'roi.lh.f13.w-vs-o.label', 'roi.lh.f20.f-vs-o.label', 'roi.lh.f13.f-vs-w.label';
+    'roi.lh.f13.w-vs-o.label', 'roi.lh.f13.f-vs-o.label', 'roi.lh.f20.f-vs-w.label';
+    'roi.lh.f13.w-vs-o.label', 'roi.lh.f20.f-vs-o.label', 'roi.lh.f13.f-vs-w.label';
     'roi.lh.f13.w-vs-o.label', 'roi.lh.f13.f-vs-o.ffa1.label', 'roi.lh.f20.f-vs-w.label';
     'roi.lh.f13.w-vs-o.label', 'roi.lh.f13.f-vs-o.ffa2.label', 'roi.lh.f20.f-vs-w.label'
     };
@@ -71,28 +71,59 @@ fs_fun_screenshot_label(FW, label_multi, output_path, overlay);
 
 
 %% Classifications with CoSMoMVPA
-% labelNames = {'roi.rh.f20.f-vs-o.label'};
-classifiers = 1; % 1:nclassifiers
+labelList = { % ...
+    'roi.lh.f13.o-vs-scr.label';
+    'roi.rh.f13.o-vs-scr.label';
+    'roi.lh.f13.f-vs-o.ffa1.label';
+    'roi.lh.f20.f-vs-o.ffa1.label';
+    'roi.lh.f13.f-vs-o.ffa2.label';
+    'roi.lh.f13.w-vs-o.label';
+    'roi.lh.f13.f-vs-o.label';
+    'roi.lh.f20.f-vs-o.label';
+    'roi.lh.f40.f-vs-o.label';
+    'roi.rh.f13.f-vs-o.ffa1.label';
+    'roi.rh.f13.f-vs-o.ffa2.label';
+    'roi.rh.f20.f-vs-o.ffa2.label';
+    'roi.rh.f40.f-vs-o.ffa2.label';
+    'roi.rh.f13.f-vs-o.label';
+    'roi.rh.f20.f-vs-o.label';
+    'roi.rh.f40.f-vs-o.label'
+    };
+
+% define the pairs for classification
+classifyPairs_E1 = {'face_intact', 'word_intact';
+    'face_intact', 'face_exchange';
+    'word_intact', 'word_exchange';
+    'face_top', 'face_bottom';
+    'word_top', 'word_bottom';
+    };
+
+classifyPairs_E2 = {'Chinese_intact', 'English_intact';
+    'Chinese_intact', 'Chinese_exchange';
+    'Chinese_top', 'Chinese_bottom';
+    'English_intact', 'English_exchange';
+    'English_top', 'English_bottom'};
+classPairs = [classifyPairs_E1; classifyPairs_E2];
+
 % 1-libsvm; 2-nn; 3-naive bayes; 4-lda; 5-svm(matlab)
+classifiers = 1; % 1:nclassifiers 
 runLoc = 0; % run analysis for localizer
 
 % run the analysis
-[mvpaTable, uniTable, uniLocTable] = fw_classification(labelList, classifiers, runLoc, output_path);
+% [mvpaTable, uniTable, uniLocTable] = 
+fs_fun_cosmo_classification(FW, labelList, classPairs, classifiers, runLoc, output_path);
 
 
 %% Searchlight
-% get setup for FaceWord project
-subjBoldList = FW.subjList;
-nSubj = FW.nSubj;
 
-for iSubj = 1:nSubj
-    
-    thisSubj = subjBoldList{iSubj};
-    
-    expCode = ceil(iSubj/(nSubj/2));
-    
-%     feature_count = 200;
-    fw_searchlight(thisSubj, expCode);
-    
-end
+file_surfcoor = 'inflated';
+combineHemi = 3;  % for each hemipsheres separately and the whole brain
+classPairs_SL = {
+    'face_intact', 'word_intact';
+    'Chinese_intact', 'English_intact'
+    };
+classifier = 1;
+
+fs_fun_cosmo_searchlight(FW, file_surfcoor, combineHemi, classPairs_SL, classifier);
+
 
